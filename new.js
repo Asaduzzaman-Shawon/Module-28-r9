1559e6ce18ec0081e299a1f5be9aff505e0ad908
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-05 23:46:08 +0600

    pzy

--- new.js
+++ new.js
@@ -1,1 +1,43 @@
-console.log("dgh");
+document.getElementById('cash-in')
+.addEventListener('click', function(){
+    const test = showUp('cash-in-from')
+    const test2 = hideUp('cash-out-from')
+})
+
+document.getElementById('cash-out')
+.addEventListener('click', function(){
+    const test = showUp('cash-out-from');
+    const test2 = hideUp('cash-in-from')
+})
+
+document.getElementById('cash-in-confirm-btn')
+.addEventListener('click', function(e){
+    e.preventDefault()
+    const test = getTextValue('cash-in-ammount')
+    const test2 = getInputValue('account-pin');
+    if(test2 === '1234'){
+        const test3 = getTextValue('account-balance');
+        const test4 = test3 + test;
+        console.log(test4);
+        document.getElementById('account-balance').innerText = test4
+    }
+    else{
+        alert('Wrong pin')
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utilities.js
+++ utilities.js
@@ -1,1 +1,42 @@
-console.log("d");
+function showUp(id){
+    const show = document.getElementById(id).classList.remove('hidden')
+    return show;
+}
+
+function hideUp(id){
+    const hide = document.getElementById(id).classList.add('hidden');
+    return hide;
+}
+
+function getInputValue(id){
+    const a = document.getElementById(id).value;
+    return a;
+}
+
+function getTextValue(id){
+    const a = document.getElementById(id).innerText;
+    const b = parseFloat(a);
+    return b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
